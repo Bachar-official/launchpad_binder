c82367a217fd1c12b65bf1524e584103be90718c
iBacharnikov <cjba4ar@gmail.com>
2025-11-01 15:40:25 +0500

    Updated .gitignore, formatted Scope

--- lib/app/scope.dart
+++ lib/app/scope.dart
@@ -10,14 +10,41 @@ import 'package:logger/logger.dart';
 import 'package:flutter/material.dart';
 
 class AppScopeContainer extends ScopeContainer {
-  late final deps = dep(() => (logger: Logger(), scaffoldKey: GlobalKey<ScaffoldMessengerState>(), navKey: GlobalKey<NavigatorState>()));
+  late final deps = dep(
+    () => (
+      logger: Logger(),
+      scaffoldKey: GlobalKey<ScaffoldMessengerState>(),
+      navKey: GlobalKey<NavigatorState>(),
+    ),
+  );
 
   late final midiService = dep(() => MidiService());
   late final configService = dep(() => ConfigService());
 
-  late final settingsManager = dep(() => SettingsManager(SettingsState.initial(), deps: deps.get, midiService: midiService.get, configService: configService.get));
-  late final wizardManager = dep(() => WizardManager(WizardState.initial(), deps: deps.get, midiService: midiService.get, configService: configService.get));
-  late final colorManager = dep(() => ColorDictionaryManager(ColorDictionaryState.initial(), deps: deps.get, midiService: midiService.get, configService: configService.get));
+  late final settingsManager = dep(
+    () => SettingsManager(
+      SettingsState.initial(),
+      deps: deps.get,
+      midiService: midiService.get,
+      configService: configService.get,
+    ),
+  );
+  late final wizardManager = dep(
+    () => WizardManager(
+      WizardState.initial(),
+      deps: deps.get,
+      midiService: midiService.get,
+      configService: configService.get,
+    ),
+  );
+  late final colorManager = dep(
+    () => ColorDictionaryManager(
+      ColorDictionaryState.initial(),
+      deps: deps.get,
+      midiService: midiService.get,
+      configService: configService.get,
+    ),
+  );
 }
 
 class AppScopeHolder extends ScopeHolder<AppScopeContainer> {
